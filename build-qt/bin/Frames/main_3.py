3b724265c037c4b22d45f3673d1c2491882ec155
Radu Alexandru Rosu <alejandro.rosu@gmail.com>
2016-07-13 00:40:47 +0200

    Implementing a new attempt at calculating the unwrapping coeficients building on top of the sawtooth minimization algorithm

--- build-qt/bin/Frames/main_3.py
+++ build-qt/bin/Frames/main_3.py
@@ -11,7 +11,7 @@ from scipy.misc import imread
 import scipy.constants as csts
 import numpy as np
 from math import atan2, sin, cos, floor, fabs, log, exp, sqrt
-from testing_sawtooth import *
+#from testing_sawtooth import *
 
 from skimage.restoration import unwrap_phase
 from skimage import exposure
@@ -21,6 +21,67 @@ from scipy.optimize import minimize
 
 ccp = 'Greys_r'
 cca = 'Greens_r'
+
+
+
+def interpolate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+def intersect_saw  (freq, num_wraps_freq_1, val, num_points, sigma):
+    #val ist the middle point
+
+    intersects = np.ndarray((num_wraps_freq_1,num_points), dtype=np.float)
+    points=np.ndarray((num_points), dtype=np.float)
+    middle_point=int (floor (num_points/2))
+    first_dist= csts.c * (2*np.pi) /(4*np.pi*freq*1000000)        #distance at which the first unwrap will happend
+    
+    #print "first dist of", freq, " is ", first_dist
+    
+    #print "middle point is", middle_point
+    #print "val is ", val
+    
+    #fill the values for points
+    points[middle_point]=val
+    #for higher values
+    for i in range (middle_point+1,int(num_points)):
+        #print "filling high value at index", i
+        dist_middle=abs(i-middle_point)
+        #print "dist to middle is ", dist_middle
+        points[i]=val + sigma*dist_middle
+        if points[i]>2*np.pi:
+            points[i]=points[i]-2*np.pi
+        #print "high val is", points[i]
+    for i in range (0,middle_point):
+        dist_middle=abs(i-middle_point)
+        points[i]=val - sigma*dist_middle
+        if points[i]<0:
+            points[i]=2*np.pi + points[i]
+        #print "low val is", points[i]
+        #print "filling low value at index", i
+        
+    #now we have in points the values of the phases that we will intersect
+    
+    #now we get the first intersect of all of them to the first saw tooth, which is just a linear interpolation
+    for i  in range (num_points):
+        intersects[0,i]=interpolate(points[i],0,2*np.pi,0,first_dist)
+        #print "interpolation of ", points[i], " is ", intersects[0,i]
+        
+    #now the next rows of the intersects matrix is just multiples of the first_dist with respect to the first row
+    for i  in range (1,num_wraps_freq_1):
+        for col in range (num_points):
+            intersects[i,col]=intersects[0,col] + first_dist*i
+        
+    return intersects
+    
 
 def constraint_funk(x):
     if x[0] <11 and x[1] <3 and x[2] < 16 and (x>0).all():
@@ -84,9 +145,9 @@ if __name__ == "__main__":
     #        plt.subplot(1, 3, 3)
     #        plt.imshow(p0tables[:,:,2])
 
-    # with open("decodedIR", "rb") as f2:
-#    with open("../data/20160616-120946344.ir", "rb") as f2: # Boris
-    with open("../data/20160616-12434139.ir", "rb") as f2:
+    with open("decodedIR", "rb") as f2:
+    #with open("./data/20160616-120946344.ir", "rb") as f2: # Boris
+    #with open("./data/20160616-12434139.ir", "rb") as f2:
         plt.figure("Raw data")
         irpic = np.fromfile(f2, dtype=np.int32)
         print (irpic.shape)
@@ -95,9 +156,9 @@ if __name__ == "__main__":
 
         ax = plt.subplot(4, 1, 1)
 
-        #       i = imread("dumprgb12.jpeg")
+        i = imread("dumprgb12.jpeg")
 #        i = imread("../data/20160616-120946332.rgb") #Boris
-        i = imread("../data/20160616-12434225.rgb") #Corbin
+        #i = imread("../data/20160616-12434225.rgb") #Corbin
 
         ax.set_axis_off()
 
@@ -253,6 +314,7 @@ if __name__ == "__main__":
     pp1 = ((irpic[1, :, :] * np.exp(-1j * (0 + 2 * np.pi * 1. / 3))))
     pp2 = ((irpic[2, :, :] * np.exp(-1j * (0 + 2 * np.pi * 2. / 3))))
     phases[:, :, 0] = - np.arctan2(np.real(pp0 + pp1 + pp2), np.imag(pp0 + pp1 + pp2))
+    phases[:, :, 0] = phases[:, :, 0]+np.pi  #do it so as to get values from 0 to 2*pi and not -pi to pi
 
     #    pp3 = ((irpic[3, :, :] * np.exp(-1j * ((p0tables[:,:,1]* -0.000031 * np.pi)))))
     #    pp4 = ((irpic[4, :, :] * np.exp(-1j * (p0tables[:,:,1]* -0.000031 * np.pi + 2 * np.pi * 1. / 3))))
@@ -262,84 +324,86 @@ if __name__ == "__main__":
     pp5 = ((irpic[5, :, :] * np.exp(-1j * (0 + 2 * np.pi * 2. / 3))))
     #    print - np.angle(pp3+pp4+pp5)
     phases[:, :, 1] = - np.arctan2(np.real(pp3 + pp4 + pp5), np.imag(pp3 + pp4 + pp5))
+    phases[:, :, 1] = phases[:, :, 1]+np.pi  #do it so as to get values from 0 to 2*pi and not -pi to pi
 
     pp6 = ((irpic[6, :, :] * np.exp(-1j * (0))))
     pp7 = ((irpic[7, :, :] * np.exp(-1j * (0 + 2 * np.pi * 1. / 3))))
     pp8 = ((irpic[8, :, :] * np.exp(-1j * (0 + 2 * np.pi * 2. / 3))))
     #    print - np.angle(pp0+pp1+pp2)
     phases[:, :, 2] = - np.arctan2(np.real(pp6 + pp7 + pp8), np.imag(pp6 + pp7 + pp8))
+    phases[:, :, 2] = phases[:, :, 2]+np.pi  #do it so as to get values from 0 to 2*pi and not -pi to pi
 
     amps[:, :, 0] = 2. / 3. * np.absolute(pp0 + pp1 + pp2)
     amps[:, :, 1] = 2. / 3. * np.absolute(pp2 + pp3 + pp4)
     amps[:, :, 2] = 2. / 3. * np.absolute(pp5 + pp6 + pp7)
-    
-
-    cons = ({'type': 'eq', 'fun': constraint_funk})
-    unwrap_coeff = np.asarray(phases, dtype=np.float32)
-#    for row in range(424):
-#        for col in range(512):
-#            pixel_phases =phases[row,col,:]
-#            res = min_ob_funk(ob_funk, pixel_phases)
-##            res = minimize(ob_funk, [2., 2., 2.], constraints=cons, args=(pixel_phases,))
-#            unwrap_coeff[row,col,:] = res
-##            print res
-#        print "Now processing: ", row, col
-#    with open("unwrap_coeff.dat", 'w') as n_pha:
-#        np.save(n_pha, unwrap_coeff)
-        
-    unwrap_coeff = np.load("unwrap_coeff.dat")
-    
-    plt.figure("Unwrapping Coeffs")
-    plt.subplot(1, 3, 1)
-    plt.imshow(unwrap_coeff[:,:,0],cmap='Greys_r')
-    plt.subplot(1, 3, 2)
-    plt.imshow(unwrap_coeff[:,:,1],cmap='Greys_r')
-    plt.subplot(1, 3, 3)
-    plt.imshow(unwrap_coeff[:,:,2],cmap='Greys_r')
-
-    depths = np.asarray(phases,dtype=np.float32)
-    
-    depths = (phases + 2*np.pi*unwrap_coeff)
-    
-    depths[:,:,0] = depths[:,:,0] / (4*np.pi*80)  
-    depths[:,:,1] = depths[:,:,1] / (4*np.pi*16)
-    depths[:,:,2] = depths[:,:,2] / (4*np.pi*120)
-
-    plt.figure("depths")
-    plt.subplot(1, 3, 1)
-    plt.imshow(depths[:,:,0],cmap='Greens_r')
-    plt.subplot(1, 3, 2)
-    plt.imshow(depths[:,:,1],cmap='Greens_r')
-    plt.subplot(1, 3, 3)
-    plt.imshow(depths[:,:,2],cmap='Greens_r')
-    
-    depth_map = np.zeros((424,512), dtype=np.float32)
-    depth_map = depths.mean(axis=2)
-    
-    plt.figure("Depth map")
-    plt.imshow(depth_map)
-
-    scaled_phases = np.asarray(phases,dtype=np.float32)
-    scaled_phases = phases + 2*np.pi*unwrap_coeff
-    
-    scaled_phases[:,:,0] = scaled_phases[:,:,0] / (3. * (1./80) / 2. * np.pi)
-    scaled_phases[:,:,1] = scaled_phases[:,:,1] / (15. * (1./16) / 2. * np.pi)
-    scaled_phases[:,:,2] = scaled_phases[:,:,2] / (2. * (1./120) / 2. * np.pi)
-    
-    tof = np.zeros((424,512), dtype=np.float32)
-    tof= np.sum(scaled_phases, axis=2) / (1./(3. * (1./80) / 2. * np.pi) + 1./(15. * (1./16) / 2. * np.pi) + 1./(2. * (1./120) / 2. * np.pi))
-    
-    plt.figure("tof")
-    plt.imshow(tof)    
-    
-    
-    plt.figure("Scaled")
-    plt.subplot(1, 3, 1)
-    plt.imshow(scaled_phases[:,:,0],cmap='Greys_r')
-    plt.subplot(1, 3, 2)
-    plt.imshow(scaled_phases[:,:,1],cmap='Greys_r')
-    plt.subplot(1, 3, 3)
-    plt.imshow(scaled_phases[:,:,2],cmap='Greys_r')
+#    
+#
+#    cons = ({'type': 'eq', 'fun': constraint_funk})
+#    unwrap_coeff = np.asarray(phases, dtype=np.float32)
+##    for row in range(424):
+##        for col in range(512):
+##            pixel_phases =phases[row,col,:]
+##            res = min_ob_funk(ob_funk, pixel_phases)
+###            res = minimize(ob_funk, [2., 2., 2.], constraints=cons, args=(pixel_phases,))
+##            unwrap_coeff[row,col,:] = res
+###            print res
+##        print "Now processing: ", row, col
+##    with open("unwrap_coeff.dat", 'w') as n_pha:
+##        np.save(n_pha, unwrap_coeff)
+#        
+#    unwrap_coeff = np.load("unwrap_coeff.dat")
+#    
+#    plt.figure("Unwrapping Coeffs")
+#    plt.subplot(1, 3, 1)
+#    plt.imshow(unwrap_coeff[:,:,0],cmap='Greys_r')
+#    plt.subplot(1, 3, 2)
+#    plt.imshow(unwrap_coeff[:,:,1],cmap='Greys_r')
+#    plt.subplot(1, 3, 3)
+#    plt.imshow(unwrap_coeff[:,:,2],cmap='Greys_r')
+#
+#    depths = np.asarray(phases,dtype=np.float32)
+#    
+#    depths = (phases + 2*np.pi*unwrap_coeff)
+#    
+#    depths[:,:,0] = depths[:,:,0] / (4*np.pi*80)  
+#    depths[:,:,1] = depths[:,:,1] / (4*np.pi*16)
+#    depths[:,:,2] = depths[:,:,2] / (4*np.pi*120)
+#
+#    plt.figure("depths")
+#    plt.subplot(1, 3, 1)
+#    plt.imshow(depths[:,:,0],cmap='Greens_r')
+#    plt.subplot(1, 3, 2)
+#    plt.imshow(depths[:,:,1],cmap='Greens_r')
+#    plt.subplot(1, 3, 3)
+#    plt.imshow(depths[:,:,2],cmap='Greens_r')
+#    
+#    depth_map = np.zeros((424,512), dtype=np.float32)
+#    depth_map = depths.mean(axis=2)
+#    
+#    plt.figure("Depth map")
+#    plt.imshow(depth_map)
+#
+#    scaled_phases = np.asarray(phases,dtype=np.float32)
+#    scaled_phases = phases + 2*np.pi*unwrap_coeff
+#    
+#    scaled_phases[:,:,0] = scaled_phases[:,:,0] / (3. * (1./80) / 2. * np.pi)
+#    scaled_phases[:,:,1] = scaled_phases[:,:,1] / (15. * (1./16) / 2. * np.pi)
+#    scaled_phases[:,:,2] = scaled_phases[:,:,2] / (2. * (1./120) / 2. * np.pi)
+#    
+#    tof = np.zeros((424,512), dtype=np.float32)
+#    tof= np.sum(scaled_phases, axis=2) / (1./(3. * (1./80) / 2. * np.pi) + 1./(15. * (1./16) / 2. * np.pi) + 1./(2. * (1./120) / 2. * np.pi))
+#    
+#    plt.figure("tof")
+#    plt.imshow(tof)    
+#    
+#    
+#    plt.figure("Scaled")
+#    plt.subplot(1, 3, 1)
+#    plt.imshow(scaled_phases[:,:,0],cmap='Greys_r')
+#    plt.subplot(1, 3, 2)
+#    plt.imshow(scaled_phases[:,:,1],cmap='Greys_r')
+#    plt.subplot(1, 3, 3)
+#    plt.imshow(scaled_phases[:,:,2],cmap='Greys_r')
 #    T = np.asarray(phases, dtype=np.float32)
 #    T[:, :, 0] = phases[:, :, 0] * 3.0 / (2. * np.pi)
 #    T[:, :, 1] = phases[:, :, 1] * 15.0 / (2. * np.pi)
@@ -391,6 +455,73 @@ if __name__ == "__main__":
     #    ax1.imshow(ir_filtered, cmap='Greens_r')
 
     # print ir_filtered
+
+
+
+
+
+
+    # %%
+    ## Attempt 1 at unwrapping using the idea of sawtooth minimization 
+    num_points=1   # num of points that will be intersected. should be odd
+    num_wraps_freq_1=10    #num of teeth the saw signal has
+    num_wraps_freq_2=2
+    num_wraps_freq_3=15    
+    sigma=0.1              #distance between points
+    
+    attempt_1_depth = np.ndarray((424, 512), dtype=np.float)
+
+    for row in range(424):
+        for col in range(512):
+            #intersect, the saw tih 80mhz with the phase val, retruning 10 points of intersections with 2 points of variance, of 0.1 stepsize and
+            val_80 =phases[row, col, 0]
+            val_16 =phases[row, col, 1]
+            val_120=phases[row, col, 2]
+            intersections_80 = intersect_saw  (80, num_wraps_freq_1, val_80, num_points, sigma)  
+            intersections_16 = intersect_saw  (16, num_wraps_freq_2, val_16, num_points, sigma)  
+            intersections_120= intersect_saw (120, num_wraps_freq_3, val_120, num_points, sigma)  
+            
+            
+            #search through the 3 tables and find values that are closest
+            #6 indices, 2 for each table
+            num_rows_80 = intersections_80.shape[0]
+            num_cols_80 = intersections_80.shape[1]
+            
+            num_rows_16 = intersections_16.shape[0]
+            num_cols_16 = intersections_16.shape[1]
+            
+            num_rows_120 = intersections_120.shape[0]
+            num_cols_120 = intersections_120.shape[1]
+            
+            difference=100000
+            n_0=100
+            n_1=100
+            n_2=100
+            
+            mean_dist=100
+            
+#            print "starting the big loops"
+#            for row_80 in range (num_rows_80):
+#                for col_80 in range (num_cols_80):
+#                    for row_16 in range (num_rows_16):
+#                        for col_16 in range (num_cols_16):
+#                            for row_120 in range (num_rows_120):
+#                                for col_120 in range (num_cols_120):
+#                                    dif=abs(abs(intersections_80[row_80,col_80]-intersections_16[row_16,col_16]) - intersections_120[row_120,col_120])
+#                                    print "dif is", dif
+#                                    if (dif< difference):
+#                                        difference=dif
+#                                        mean_dist=(intersections_80[row_80,col_80] +intersections_16[row_16,col_16]+ intersections_120[row_120,col_120])/3
+##                                        
+#                                    
+                                    
+            print "mean_dist is", mean_dist, "with difference", difference
+            attempt_1_depth[row,col]=mean_dist
+#            print "num_rows",num_rows_120
+#            print "num_cols",num_cols_120
+    
+
+            
 
 
     # %%
@@ -465,18 +596,18 @@ if __name__ == "__main__":
 
     print "phases.shape:", phases.shape
 
-    unwrapped_ = unwrap_superior(medfilt(phases + np.pi))
-    plt.figure("superior")
-    plt.imshow((unwrapped_), cmap=ccp)
-    #    plt.subplot(1, 3, 1)
-    #    plt.imshow((unwrapped_[:,:,0]), cmap=ccp)
-    #    plt.subplot(1, 3, 2)
-    #    plt.imshow((unwrapped_[:,:,1]), cmap=ccp)
-    #    plt.subplot(1, 3, 3)
-    #    plt.imshow((unwrapped_[:,:,2]), cmap=ccp)
-    print "unwrapped: ", unwrapped_.shape, unwrapped_.max(), unwrapped_.min()
-    with open("unwrapped.dat", 'w') as unw:
-        np.save(unw, unwrapped_)
-
-    plt.show()
+#    unwrapped_ = unwrap_superior(medfilt(phases + np.pi))
+#    plt.figure("superior")
+#    plt.imshow((unwrapped_), cmap=ccp)
+#    #    plt.subplot(1, 3, 1)
+#    #    plt.imshow((unwrapped_[:,:,0]), cmap=ccp)
+#    #    plt.subplot(1, 3, 2)
+#    #    plt.imshow((unwrapped_[:,:,1]), cmap=ccp)
+#    #    plt.subplot(1, 3, 3)
+#    #    plt.imshow((unwrapped_[:,:,2]), cmap=ccp)
+#    print "unwrapped: ", unwrapped_.shape, unwrapped_.max(), unwrapped_.min()
+#    with open("unwrapped.dat", 'w') as unw:
+#        np.save(unw, unwrapped_)
+#
+#    plt.show()
     print "finished"
